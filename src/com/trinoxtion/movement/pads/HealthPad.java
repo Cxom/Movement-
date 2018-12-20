c5d926837acc432d2885df7d2b9f57e23fadf899
Cxom <cxomtdoh@yahoo.com>
2018-12-20 06:35:03 -0500

    Refactor HealthPad and ElytraPad

--- src/com/trinoxtion/movement/pads/HealthPad.java
+++ src/com/trinoxtion/movement/pads/HealthPad.java
@@ -6,7 +6,6 @@ import java.util.Set;
 import org.bukkit.Material;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
@@ -17,13 +16,10 @@ public class HealthPad extends SimplePad implements Listener {
 
 	public static final HealthPad DEFAULT = new HealthPad(Material.QUARTZ_BLOCK, (byte) 1);
 	
-	//This is for cancelling, but is it necessary or just cleaned on reload regardless? How is an async task time stopped?
-	//TODO
-	@SuppressWarnings("unused")
-	private final BukkitTask healthTask;
+	private static final double DEFAULT_RATE_IN_SECONDS = 0.6;
 	
+	private BukkitTask healthTask;
 	private final long rateInTicks;
-	
 	private Set<MovementPlayer> playersOnPads = new HashSet<>();
 	
 	public HealthPad(Material material){
@@ -31,7 +27,7 @@ public class HealthPad extends SimplePad implements Listener {
 	}
 	
 	public HealthPad(Material material, byte data) {
-		this(material, data, 0.6);
+		this(material, data, DEFAULT_RATE_IN_SECONDS);
 	}
 	
 	public HealthPad(Material material, byte data, double rateInSeconds){
@@ -40,30 +36,34 @@ public class HealthPad extends SimplePad implements Listener {
 
 		MovementPlusPlus.registerEvents(this);
 		
-		healthTask = new BukkitRunnable(){
-			public void run(){
-				playersOnPads.forEach(mp -> {
-					mp.getPlayer().setHealth(Math.min(mp.getPlayer().getHealth() + 0.5, 20));
-				});
-			}	
-		}.runTaskTimerAsynchronously(MovementPlusPlus.getPlugin(), rateInTicks / 2, rateInTicks);
+		startHealthTask();
 	}
 	
-//	@EventHandler
-//	public void onPlayerMove(PlayerMoveEvent e) {
-//		Player player = e.getPlayer();
-//		Bukkit.broadcastMessage(player.getName() + " uses this component: " + usesThisComponent(player));
-//		if (usesThisComponent(player)){
-//			MovementPlayer mp = MovementPlusPlus.getMovementPlayer(player);
-//			if (player.getLocation().getBlock().getRelative(0, -1, 0).getType() == this.material) {
-//				playersOnPads.add(mp);
-//				Bukkit.broadcastMessage(player.getName() + " added to healthpad list");
-//			} else {
-//				playersOnPads.remove(mp);
-//				Bukkit.broadcastMessage(player.getName() + " removed from healthpad list");
-//			}
-//		}
-//	}
+	
+	public void startHealthTask() {
+		if (healthTask == null) {
+			healthTask = new BukkitRunnable(){
+				public void run(){
+					doHealthPadTick();
+				}	
+			}.runTaskTimerAsynchronously(MovementPlusPlus.getPlugin(), rateInTicks / 2, rateInTicks);
+		}
+	}
+	
+	public void stopHealthTask() {
+		if (healthTask != null) {
+			healthTask.cancel();
+			healthTask = null;
+		}
+	}
+	
+	private void doHealthPadTick() {
+		playersOnPads.forEach(this::incrementHealth);
+	}
+	
+	private void incrementHealth(MovementPlayer movementPlayer) {
+		movementPlayer.getPlayer().setHealth(Math.min(movementPlayer.getPlayer().getHealth() + 0.5, 20));
+	}
 	
 	@Override
 	public void onMovement(PlayerMoveEvent event, MovementPlayer mp) {
@@ -72,7 +72,6 @@ public class HealthPad extends SimplePad implements Listener {
 		} else {
 			playersOnPads.remove(mp);
 		}
-		
 	}
 	
 	@Override
@@ -85,7 +84,5 @@ public class HealthPad extends SimplePad implements Listener {
 		playersOnPads.remove(mp);
 	}
 
-	
-	
 }
 
